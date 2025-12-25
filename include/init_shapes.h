8d2ef3501348fb58fe8af1c6da934a306aeb7db9
yujaw <yujawmanandhar@gmail.com>
2025-12-25 00:34:08 -0500

    Original Files

--- include/init_shapes.h
+++ include/init_shapes.h
@@ -20,38 +20,6 @@ typedef struct
     float y2;
 } Line;
 
-typedef enum
-{
-    SHAPE_LINE,
-    SHAPE_CIRCLE,
-    SHAPE_QUAD
-} ShapeType;
-
-typedef struct
-{
-    int id;
-    ShapeType type;
-
-    Vec2 acceleration;
-    Vec2 velocity;
-
-    float mass;
-    float restitution;
-    float friction;
-    bool isDynamic;
-
-    union
-    {
-        struct
-        {
-            float x, y, r;
-        } circle;
-        struct
-        {
-            Vec2 vertices[2];
-        } line;
-    } data;
-} Body;
 
 Circle init_circle(int id, float x, float y, float r);
 Line init_line(int id, float x1, float y1, float x2, float y2);
